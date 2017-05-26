ff31cb15e2556089d11d75895365e4b5c578151d
RamirezD <sard1966@hotmail.com>
2017-05-26 18:05:59 -0500

    Segundo Commit

--- encriptado1.c
+++ encriptado1.c
@@ -26,24 +26,61 @@ void encriptadociclico(int c, char *p){
 		if (validacion == 0){encriptado[i] = p[i];}
 	}
 	printf(encriptado);
-	encriptadomorse("krod shgur");
+	encriptadomorse(encriptado);
 }
 
-void encriptadomorse(char p){
+void encriptadomorse(char p[] ){
 	char encriptado[1024] = {0};
 	int tamanio, i, j, validacion;
 	char vowels[][37] = {
 	    {'a', 'b', 'c', 'd', 'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ',0,1,2,3,4,5,6,7,8,9},
 	    {'A', 'B', 'C', 'D', 'E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',' ',0,1,2,3,4,5,6,7,8,9},
 	};
-char morse[60] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..","/",".----","..---","...--","....-","-....","--...","---..","----.","-----"};
-
+char morse[60] = {0}; 
+morse[0] = ".-";
+morse[1] = "-...";
+morse[2] = "-.-.";
+morse[3] = "-..";
+morse[4] = ".";
+morse[5] = "..-.";
+morse[6] = "--.";
+morse[7] = "....";
+morse[8] = "..";
+morse[9] = ".---";
+morse[10] = "-.-";
+morse[11] = ".-..";
+morse[12] = "--";
+morse[13] = "-.";
+morse[14] = "---";
+morse[15] = ".--.";
+morse[16] = "--.-";
+morse[17] = ".-.";
+morse[18] = "...";
+morse[19] = "-";
+morse[20] = "..-";
+morse[21] = "...-";
+morse[22] = ".--";
+morse[23] = "-..-";
+morse[24] = "-.--";
+morse[25] = "--..";
+morse[26] = "/";
+morse[27] = ".----";
+morse[28] = "..---";
+morse[29] = "...--";
+morse[30] = "....-";
+morse[31] = ".....";
+morse[32] = "-....";
+morse[33] = "--...";
+morse[34] = "---..";
+morse[35] = "----.";
+morse[36] = "-----";
 	tamanio = strlen(p);
 	for (i = 0; i < tamanio; i++){
 		validacion = 0;
 		for(j = 0; j < 37; j++){
 			if(p[i] == vowels[0][j] || p[i] == vowels[1][j]){
 				encriptado[i] = morse[j];
+				printf(morse[j]);
 				validacion = 1;
 			}
 		}
