a452614edc3e96cab352cc2d959237e5ae29304b
RamirezD <sard1966@hotmail.com>
2017-05-26 18:40:10 -0500

    Ejercicio terminado, commit final

--- encriptado1.c
+++ encriptado1.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 void encriptadociclico(int c, char *p){
 	
 	char encriptado[1024]= {0};
@@ -25,68 +26,32 @@ void encriptadociclico(int c, char *p){
 		}
 		if (validacion == 0){encriptado[i] = p[i];}
 	}
+	printf("\nMensaje encriptado: ");
 	printf(encriptado);
+	printf("\nMensaje en codigo morse: ");
 	encriptadomorse(encriptado);
 }
 
-void encriptadomorse(char p[] ){
+void encriptadomorse(char *p){
 	char encriptado[1024] = {0};
 	int tamanio, i, j, validacion;
-	char vowels[][37] = {
-	    {'a', 'b', 'c', 'd', 'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ',0,1,2,3,4,5,6,7,8,9},
-	    {'A', 'B', 'C', 'D', 'E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',' ',0,1,2,3,4,5,6,7,8,9},
-	};
-char morse[60] = {0}; 
-morse[0] = ".-";
-morse[1] = "-...";
-morse[2] = "-.-.";
-morse[3] = "-..";
-morse[4] = ".";
-morse[5] = "..-.";
-morse[6] = "--.";
-morse[7] = "....";
-morse[8] = "..";
-morse[9] = ".---";
-morse[10] = "-.-";
-morse[11] = ".-..";
-morse[12] = "--";
-morse[13] = "-.";
-morse[14] = "---";
-morse[15] = ".--.";
-morse[16] = "--.-";
-morse[17] = ".-.";
-morse[18] = "...";
-morse[19] = "-";
-morse[20] = "..-";
-morse[21] = "...-";
-morse[22] = ".--";
-morse[23] = "-..-";
-morse[24] = "-.--";
-morse[25] = "--..";
-morse[26] = "/";
-morse[27] = ".----";
-morse[28] = "..---";
-morse[29] = "...--";
-morse[30] = "....-";
-morse[31] = ".....";
-morse[32] = "-....";
-morse[33] = "--...";
-morse[34] = "---..";
-morse[35] = "----.";
-morse[36] = "-----";
+	char abecedario[60] = "abcdefghijklmnopqrstuvwxyz 0123456789";
+	char ABECEDARIO[60] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789";
+	char morse[60][40]={".-","—···","-·-·","-··",".","··-·","--·","····","··","·---","-·-","·-··","--","-·","---","·--·",
+			    "--·-","·-·","···","-","··-","···-","·--","-··-","-·--","--··","/","·----","··---","···--","····-","·····",
+	  		    "-····","--···","---··","----·","-----"};
+
 	tamanio = strlen(p);
 	for (i = 0; i < tamanio; i++){
 		validacion = 0;
-		for(j = 0; j < 37; j++){
-			if(p[i] == vowels[0][j] || p[i] == vowels[1][j]){
-				encriptado[i] = morse[j];
-				printf(morse[j]);
+		for(j = 0; j < strlen(abecedario); j++){
+			if(p[i] == abecedario[j] || p[i] == ABECEDARIO[j]){
+				printf("%s",morse[j]);
 				validacion = 1;
 			}
 		}
-		if (validacion == 0){encriptado[i] = p[i];}
+		if (validacion == 0){printf("%s",morse[j]);}
 	}
-	printf(encriptado);
 }
 
 int main(int argc, char *argv[]){
