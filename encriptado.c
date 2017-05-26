ec458768977c369581a891e86a1358b91ae9bd85
RamirezD <sard1966@hotmail.com>
2017-05-26 16:36:00 -0500

    segundo commit

--- encriptado.c
+++ encriptado.c
@@ -3,34 +3,44 @@
 #include <stdlib.h>
 #include <string.h>
 
-void encriptadociclico(){
-	char palabra[1024]={0}, encriptado[1024]= {0};
+void encriptadociclico(int c, char *p){
+	
+	char encriptado[1024]= {0};
 	char abecedario[60]="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	int tamanioAbc, codigo, tamanioP, i, j, validacion;
+	int tamanioAbc, tamanioP, i, j, validacion;
 	tamanioAbc = strlen(abecedario);
-	printf("Ingrese el mensaje a codificar: ");
-	gets(palabra);
-	printf("Ingrese el codigo numerico: ");
-	scanf("%d", &codigo);
-	tamanioP = strlen(palabra);
+	tamanioP = strlen(p);
 	for (i = 0; i < tamanioP; i++){		
 		validacion = 0;
 		for (j = 0; j < tamanioAbc; j++){
-			if(palabra[i] == abecedario[j]){
-				if(j > 25- codigo && j < 26 || j > 51-codigo){
-					encriptado[i] = abecedario[j+codigo-26];
+			if(p[i] == abecedario[j]){
+				if(j > 25- c && j < 26 || j > 51-c){
+					encriptado[i] = abecedario[j+c-26];
 				}
 				else{
-					encriptado[i] = abecedario[j+codigo];
+					encriptado[i] = abecedario[j+c];
 				}
 				validacion = 1;
 			}
 		}
-		if (validacion == 0){encriptado[i] = palabra[i];}
+		if (validacion == 0){encriptado[i] = p[i];}
 	}
 	printf(encriptado);
 }
 
-int main(void){
-	encriptadociclico();
+int main(int argc, char *argv[]){
+	char palabra[1024]={0};
+	int codigo;
+	if(argc == 3){
+		codigo = atoi(argv[1]);
+		encriptadociclico(codigo,argv[2]);
+	}
+	else{
+		printf("Ingrese el mensaje a codificar: ");
+		gets(palabra);
+		printf("Ingrese el codigo numerico: ");
+		scanf("%d", &codigo);
+		encriptadociclico(codigo, palabra);
+	}
+	return 0;
 }
